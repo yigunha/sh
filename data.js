8751479556651ea8c0597cdd05afd37cc40f0b0c
YiGunHa <isinegajok@naver.com>
2024-11-29 11:54:40 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,1 +1,7 @@
-export let months = ['Jan', 'Feb', 'Mar','Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+export let months = ['Jan', 'Feb', 'Mar', 'Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+export function getMonth(index) {
+    return months[index] || 'Invalid month';
+}
+
+export const year = 2024;
